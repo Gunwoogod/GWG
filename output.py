a7a0b03c39f06597641c5eecbe627bf090a7d0c4
Gunwoo Kim <kmogpoo1@naver.com>
2022-03-07 00:32:48 +0900

    first commit

---
+++ output.py
@@ -0,0 +1,15 @@
+#하나만 출력
+print("Hello Python progamming!")
+print()
+
+#여러개 출력
+print(11,22,33,44,55)
+print("안녕하세요." , "저는" , "김건우", "입니다.")
+print()
+
+# 줄바꿈 하기
+print("------")
+print()
+print()
+print()
+print("------")
