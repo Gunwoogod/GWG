7f6a2c425a199e4820dfc7601c008036dafddf9f
Gunwoogod <100673982+Gunwoogod@users.noreply.github.com>
2022-03-09 20:53:54 +0900

    Add files via upload

---
+++ number.py
@@ -0,0 +1,29 @@
+#숫자의 종류
+print(273)
+print(type(273))
+print()
+print(52.273)
+print(type(52.273))
+print()
+
+#숫자 연산자
+print("5+7 = " , 5+7)
+print()
+print("5-7 = " , 5-7)
+print()
+print("5*7 = " , 5*7)
+print()
+print("5/7 = " , 5/7)
+print()
+
+#생소한 연산자
+print("5//7" , 5//7)           #정수 부분만 남김
+print()
+print("5%7" , 5%7)             #나머지 부분만 남김
+print()
+print("2**1" , 2**1)           #제곱 연산자
+print("2**2" , 2**2)
+print("2**3" , 2**3)
+print()
+
+#
